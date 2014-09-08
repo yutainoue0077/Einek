2fe73596bca2a149883dabec514dded71c480b16
yutainoue <inoue_yuta@nexway.co.jp>
2014-09-08 18:57:16 +0900

    指定回数だけ演奏会情報を取得

--- app/controllers/concerts_controller.rb
+++ app/controllers/concerts_controller.rb
@@ -16,60 +16,79 @@ class ConcertsController < ApplicationController
   # GET /concerts/1
   # GET /concerts/1.json
   def show
-    driver = Selenium::WebDriver.for :safari
-    driver.navigate.to "http://www2s.biglobe.ne.jp/~jim/freude/calendar/2014oct.html"
-    driver.find_element(:xpath,'/html/body/center/table/tbody/tr/td[2]/table/tbody/tr[1]/td/table[2]/tbody/tr[5]/td[2]/a[1]').click
-    #driver.find_element(:xpath, '/html/body/center/table/tbody/tr/td[2]/table/tbody/tr[1]/td/table[2]/tbody/tr[5]/td[2]/a[2]').click
+    3.times do |i|
+
+      @concert = Concert.new
+      driver = Selenium::WebDriver.for :safari
+      driver.navigate.to "http://www2s.biglobe.ne.jp/~jim/freude/calendar/2014oct.html"
+      driver.find_element(:xpath,  "/html/body/center/table/tbody/tr/td[2]/table/tbody/tr[1]/td/table[2]/tbody/tr[5]/td[2]/a[#{i + 1}]").click
+      #driver.find_element(:xpath, /html/body/center/table/tbody/tr/td[2]/table/tbody/tr[1]/td/table[2]/tbody/tr[2]/td[2]/a[1]').click
 
 
-    html = driver.page_source
-    doc = Nokogiri.HTML(html)
-    item = Array.new(10)
+      html = driver.page_source
+      doc = Nokogiri.HTML(html)
+      item = Array.new(10)
 
-    # 演奏会タイトルの取得
-    main_title = doc.xpath('//tr/td/b').text.gsub("　", "")
-    sub_title = doc.xpath('//tr/td/p/b').text.gsub("　", "")
-    full_title = "#{main_title}【#{sub_title}】"
-    @concert.name = full_title
+      # 演奏会タイトルの取得
+      main_title = doc.xpath('/html/body/center/table/tbody/tr[1]/td/b/font').text.gsub("　", "")
+      sub_title = doc.xpath('/html/body/center/table/tbody/tr[1]/td/p/b/font').text.gsub("　", "")
+      full_title = "#{main_title}【#{sub_title}】"
+      if full_title.empty?
+        @concert.name = "なし"
+      else
+        @concert.name = full_title
+      end
 
-    # 日時・場所・を取得
-    doc.xpath('//blockquote/p').each_with_index do |node, i|
-      item[i] = node.text
+      # 日時・場所・を取得
+      doc.xpath('/html/body/center/table/tbody/tr[3]/td/blockquote/p').each_with_index do |node, i|
+        item[i] = node.text
+      end
+      item_0 = item[0]
+      if item_0.empty?
+        @concert.program = "なし"
+      else
+        @concert.program = item[0]
+      end
+
+      #場所情報をホール名のみに変更
+      #hall_short_name = item[1].split("　")
+      #stage_name = hall_short_name[0].gsub("\n場所： ", "")
+      #@concert.stage = stage_name
+
+      # 演奏曲目を連結表示
+      content_all = ""
+      doc.xpath('//dd/b').each_with_index do |node, i|
+        content_all = content_all + node.text + "\n"
+      end
+      if content_all.empty?
+        @concert.content = "なし"
+      else
+        @concert.content = content_all
+      end
+
+      @concert.save
+      # お問い合わせ先を表示
+      @infomation = Infomation.new
+      #info_all = Infomation.where("oke_name = '#{main_title}'")
+      #@infomation.info = info_all[0].info
+
+
+      # 住所情報を表示
+      @access = Access.new
+      #access_all = Access.where("hall_name = '#{stage_name}'")
+      #@access.hall_name = access_all[0].hall_name
+      #@access.spot = access_all[0].spot
+      #@access.train = access_all[0].train
+
+      # デバック用
+      @infomation.info = "aaa"
+      @access.hall_name = "aaa"
+      @access.spot = "aaa"
+      @access.train = "aaa"
+
+      driver.quit
     end
-    @concert.program = item[0]
-
-    #場所情報をホール名のみに変更
-    #hall_short_name = item[1].split("　")
-    #stage_name = hall_short_name[0].gsub("\n場所： ", "")
-    #@concert.stage = stage_name
-
-    # 演奏曲目を連結表示
-    content_all = ""
-    doc.xpath('//dd/b').each_with_index do |node, i|
-      content_all = content_all + node.text + "\n"
-    end
-    @concert.content = content_all
-
-    # お問い合わせ先を表示
-    @infomation = Infomation.new
-    #info_all = Infomation.where("oke_name = '#{main_title}'")
-    #@infomation.info = info_all[0].info
-
-
-    # 住所情報を表示
-    @access = Access.new
-    #access_all = Access.where("hall_name = '#{stage_name}'")
-    #@access.hall_name = access_all[0].hall_name
-    #@access.spot = access_all[0].spot
-    #@access.train = access_all[0].train
-
-    # デバック用
-    @infomation.info = "aaa"
-    @access.hall_name = "aaa"
-    @access.spot = "aaa"
-    @access.train = "aaa"
-
-    driver.quit
+    @concerts = Concert.all
   end
 
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,6 @@
 
+Concert.create(id: '1', name: 'a', program: 'a', stage: 'a', map: 'a', information: 'a', content: 'a')
+
 Access.create(hall_name: '俺の実家', spot: '埼玉県飯能市のどっか', train: '元加治駅から徒歩15分')
 Access.create(hall_name: 'どこか', spot: 'どこか', train: 'どこか')
 Access.create(hall_name: '和光市民文化センター', spot: '和光市', train: '和光市駅から近場')
